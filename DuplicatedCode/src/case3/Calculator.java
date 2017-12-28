c60d3f53f4c8842cb4ac12115ad7514f4360eaed
CynthiaAG <cynthia.cjag@gmail.com>
2017-12-28 17:44:59 +0000

    Kata Refactoring

---
+++ DuplicatedCode/src/case3/Calculator.java
@@ -0,0 +1,25 @@
+package case3;
+
+
+/*
+ * @Author: Cynthia Afonso
+ */
+
+public class Calculator {
+
+    public long sum(int min, int max) {
+        long result = 0;
+        for (int i = min ; i <= max ; i++)
+            result += i;
+        return result;
+    }
+
+    public long sumOfSquares(int min, int max) {
+        long result = 0;
+        for (int i = min ; i <= max ; i++)
+            result += i * i;
+        return result;
+    }
+
+}
+
